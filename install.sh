1427b2bd568f51c8e5db88edbbfff1f0794aac3c
Bruno Melo <bruno94@outlook.com>
2023-08-25 15:44:13 -0300

    add installw

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+# backup 
+backup_folder="$PWD/.backup/"
+gitconfig="$HOME/.gitconfig"
+zshrc="$HOME/.zshrc"
+tmux="$HOME/.tmux.conf"
+nvim_folder="$HOME/.config/nvim"
+
+if [ ! -d "$backup_folder" ]; then
+  mkdir -p "$backup_folder" || echo "Error backup folder"
+fi
+
+# link backup files
+files=($gitconfig $zshrc $tmux)
+# files=($nvim_folder) need fix when its a LinkSymbolic
+
+for file in "${files[@]}"; do
+    if [ -L "$file" ]; then
+        rm $file
+    else
+      if [ -d "$file" ]; then
+        cp -r $file $backup_folder
+      else
+        cp $file $backup_folder
+      fi
+      rm -rf $file
+    fi
+done
+
+# git
+ln -sf "$PWD/configs/.gitconfig" ~/
+
+# zsh
+ln -sf "$PWD/configs/.zshrc" ~/
+
+# tmux
+ln -sf "$PWD/tmux/.tmux.conf" ~/
+
+# nvim
+ln -sf "$PWD/nvim" ~/.config
+
+
+echo "installed 🔥"
