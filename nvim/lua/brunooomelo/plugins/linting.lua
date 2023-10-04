1fd90e27e50dddbca9c1500e666707266636c1fb
Bruno Melo <bruno94@outlook.com>
2023-10-04 17:53:32 -0300

    fix(linting): list in setup

--- nvim/lua/brunooomelo/plugins/linting.lua
+++ nvim/lua/brunooomelo/plugins/linting.lua
@@ -17,12 +17,11 @@ return {
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     vim.api.nvim_create_autocmd({
       "BufEnter", "BufWritePost", 
-      "InsertLeave", {
+      "InsertLeave"}, {
         group = lint_augroup,
         callback = function () 
           lint.try_lint()
         end
-      }
     })
 
     vim.keymap.set("n", "<leader>l", function()
