485b289d5483d4e215d9a50247dde34c87a3bc6e
Bruno Melo <bruno94@outlook.com>
2023-10-16 23:03:31 -0300

    feat(autopairs): add autopairs package and change bufferline config

--- nvim/lua/brunooomelo/plugins/bufferline.lua
+++ nvim/lua/brunooomelo/plugins/bufferline.lua
@@ -1,11 +1,6 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {},
 }
