05035085df394d273eb760b2282912492f5be9d8
Bruno Melo <bruno94@outlook.com>
2023-10-16 23:07:14 -0300

    chore(lualine): change custom colors to onelight theme and fix lint

--- nvim/lua/brunooomelo/plugins/lualine.lua
+++ nvim/lua/brunooomelo/plugins/lualine.lua
@@ -1,77 +1,35 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = true,
-  event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	lazy = true,
+	event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+	config = function()
+		local lualine = require("lualine")
+		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		local custom_onelight = require("lualine.themes.onelight")
 
-    local colors = {
-      blue = "#65D1FF",
-      green = "#3EFFDC",
-      violet = "#FF61EF",
-      yellow = "#FFDA7B",
-      red = "#FF4A4A",
-      fg = "#c3ccdc",
-      bg = "#112638",
-      inactive_bg = "#2c3043",
-    }
-
-    local my_lualine_theme = {
-      normal = {
-        a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      insert = {
-        a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      visual = {
-        a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      command = {
-        a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      replace = {
-        a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-        b = { bg = colors.bg, fg = colors.fg },
-        c = { bg = colors.bg, fg = colors.fg },
-      },
-      inactive = {
-        a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-        b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-        c = { bg = colors.inactive_bg, fg = colors.semilightgray },
-      },
-    }
-
-    -- configure lualine with modified theme
-    lualine.setup({
-      options = {
-        theme = my_lualine_theme,
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = ''},
-      },
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'filename'},
-        lualine_c = {},
-        lualine_x =  {
-          { 
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
-          }, 
-          {'branch'}},
-        lualine_y = {'filetype'},
-        lualine_z = {}
-      },
-    })
-  end,
+		-- configure lualine with modified theme
+		lualine.setup({
+			options = {
+				theme = custom_onelight,
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "filename" },
+				lualine_c = {},
+				lualine_x = {
+					{
+						lazy_status.updates,
+						cond = lazy_status.has_updates,
+						color = { fg = "#ff9e64" },
+					},
+					{ "branch" },
+				},
+				lualine_y = { "filetype" },
+				lualine_z = {},
+			},
+		})
+	end,
 }
